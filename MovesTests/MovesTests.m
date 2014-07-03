9c913ef55dc0c7bbd61813be534c8629f25dbdfb
Billy Roh <billy.roh@gmail.com>
2014-07-03 13:31:02 -0700

    Initial Commit

---
+++ MovesTests/MovesTests.m
@@ -0,0 +1,34 @@
+//
+//  MovesTests.m
+//  MovesTests
+//
+//  Created by Billy Roh on 7/3/14.
+//  Copyright (c) 2014 Billy Roh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MovesTests : XCTestCase
+
+@end
+
+@implementation MovesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
